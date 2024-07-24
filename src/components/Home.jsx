551beb4f2ab7eadb1392b97df4f7be0131a6ed58
Charlisteketee <charlisteketee@gmail.com>
2024-07-24 14:09:08 -0700

    Add background img to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import '../styles/Home.css';
+import backgroundImage from '../styles/background.jpg';
 
 const Home = () => {
   return (
-    <div className="homepage">
-      <h1>Welcome!</h1>
-      <p>Explore amazing trips and adventures around the world through stories, images and connecting with others.</p>
+    <div className="homeBackground">
+      <img src={backgroundImage} alt="background" />
+      <div className="homeContent">
+        <h1>Welcome!</h1>
+        <p>Explore amazing trips and adventures around the world through stories, images, and connecting with others.</p>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="navbar">
+      <h1 className="name">"Name of app"</h1>
       <ul>
         <li><Link to="/">Home</Link></li>
+        <li><Link to="/trips">Trips</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/trips">Trips</Link></li>
       </ul>
     </nav>
   );
